7325f6ccd875b623867ad840be861fb3ea2739b5
Ian Peres Coelho <ian.coelho@MLZPC-1072.local>
2021-12-07 22:41:30 -0300

    first commit

---
+++ src/screens/signin/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+export default function SignIn() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Logar</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
---
+++ src/screens/signup/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+
+export default function SignUp() {
+  return (
+    <SafeAreaView>
+      <View style={styles.default}>
+        <View style={styles.card}>
+          <TextInput placeholder="E-mail" />
+          <TextInput placeholder="Senha" secureTextEntry={true} />
+          <TouchableOpacity>
+            <Text>Cadastrar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  default: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+  },
+  card: {
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 12,
+  },
+});
